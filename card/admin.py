8cf4dfa17c48ee8ac2b8f6533a42c6e6909eb974
workshima <vshimawork@gmail.com>
2018-04-05 16:09:14 +0300

    created settings for admin panel

--- card/admin.py
+++ card/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Product, Color, Size, Material, ProductDetail
 
 # Register your models here.
+
+
+class ProductDetailAdmin(admin.ModelAdmin):
+    list_display = ['product', 'color', 'size', 'material', 'price']
+
+
+class ProductDetailInstance(admin.TabularInline):
+    model = ProductDetail
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    inlines = [ProductDetailInstance]
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Color)
+admin.site.register(Size)
+admin.site.register(Material)
+admin.site.register(ProductDetail, ProductDetailAdmin)
